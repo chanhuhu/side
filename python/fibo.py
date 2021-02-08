a2bf139103d5851d271e96cd1c38cb2e05bf1fa0
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2021-02-09 05:26:54 +0700

    add: fn_fibo brenchmark

--- python/fibo.py
+++ python/fibo.py
@@ -1,5 +1,17 @@
 import functools
+import time
 
+def brench(func):
+    def timed(*args, **kwargs):
+        t0 = time.monotonic_ns()
+        func(*args, **kwargs)
+        t1 = time.monotonic_ns()
+        print(f"function name: {func.__name__}")
+        return (t1 - t0) * 1e-9
+    return timed
+
+
+@brench
 @functools.lru_cache(maxsize=None)
 def fibo(n: int):
     if n == 0:
@@ -9,5 +21,5 @@ def fibo(n: int):
     return fibo(n-1) + fibo(n-2)
 
 # 0 1 1 2 3 5 8 13 ..
-for i in range(10):
+for i in range(100):
     print(fibo(i))
