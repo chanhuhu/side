d0f6557ddba19e5ea9e063d0c8b37d5e9ec2aa94
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2021-02-11 17:08:07 +0700

    add: implement isleap

---
+++ interview/codium/leapyear.py
@@ -0,0 +1,35 @@
+import unittest
+import calendar
+
+INPUT_SAMPLE = """\
+1600 -> True
+2000 -> True
+1500 -> False
+2004 -> True
+2008 -> True
+2010 -> False
+"""
+
+def isleap(year: int) -> bool:
+   if year % 400 == 0:
+       return True
+
+   if year % 400 != 0 and year % 100 != 0 and year % 4 == 0:
+       return True
+
+   return False
+
+class Test(unittest.TestCase):
+
+    def test_input_sample(self):
+        lines = INPUT_SAMPLE.splitlines()
+        for line in lines:
+            input, result = line.split(" -> ")
+            input = int(input)
+            result = eval(result)
+            print(f"{input}: {isleap(input)} expected_result: {result}")
+            self.assertEqual(isleap(input),result)
+        
+
+if __name__ == "__main__":
+    unittest.main()
