42f2f6e283a469ba502c5dfb53dd4c492bb38fea
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2021-02-11 17:41:44 +0700

    add: implement triangle

---
+++ interview/codium/triangle.py
@@ -0,0 +1,27 @@
+import unittest
+
+def triangle(n: int) -> list[str]:
+    result = []
+    for i in range(1, n+1):
+        sum = ""
+        for j in range(i):
+            sum += "*"
+        result.append(sum)
+            
+    return result
+
+
+
+class Test(unittest.TestCase):
+
+    def test_input_n_equals_3(self):
+        self.assertEqual(triangle(3),["*","**","***"])
+        
+    def test_input_n_equals_4(self):
+        self.assertEqual(triangle(4),["*","**","***","****"])
+
+    def test_input_n_equals_6(self):
+        self.assertEqual(triangle(6),["*","**","***","****","*****","******"]) 
+
+if __name__ == "__main__":
+    unittest.main()
